0d84cdfd37537a1afe065d1029ff01af3020254b
sevda <imanysevda@gmail.com>
2020-07-21 19:14:15 +0430

    Add Gui class

---
+++ Gui.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class Gui extends Application {
+    
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        creatLoginPage(primaryStage);
+    }
+
+
+    public static void creatLoginPage(Stage primaryStage) throws Exception {
+    }
+}
