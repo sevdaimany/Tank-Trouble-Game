c01ec521fa178fe40a9b429d24cf8bc7defca90f
MmahdiM79 <m.mahdi.m79@gmail.com>
2020-07-25 12:36:06 +0430

    DataBase class added. version 0.0.0

---
+++ DataBase.java
@@ -0,0 +1,26 @@
+
+
+
+
+
+/**
+ * This class manage game's file and data
+ * 
+ * 
+ * @author Mohammad Mahdi Malmasi
+ * @version 0.0.0
+ */
+public class DataBase
+{
+            /*  Fields  */
+
+    // path of the main folder of files
+    private static final String MAIN_FOLDER = "./.TANK_TROUBLE_DATA_BASE/";
+
+    // path of the players data folder
+    private static final String PLAYERS_FOLDER = MAIN_FOLDER + "PLAYERS/";
+
+    // path of the photos folder
+    private static final String PHOTOS_FOLDER = MAIN_FOLDER + "PHOTOS/";
+
+}
