b3363829a43e095f8c3fe8af6ba10d1fd1738219
MmahdiM79 <m.mahdi.m79@gmail.com>
2020-07-26 17:35:41 +0430

    isRegistered method added

--- DataBase.java
+++ DataBase.java
@@ -1,6 +1,7 @@
-
-
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 
 /**
@@ -8,7 +9,7 @@
  * 
  * 
  * @author Mohammad Mahdi Malmasi
- * @version 0.0.1
+ * @version 0.1.1
  */
 public class DataBase
 {
@@ -19,6 +20,7 @@ public class DataBase
 
     // path of the players data folder
     private static final String PLAYERS_FOLDER = MAIN_FOLDER + "PLAYERS/";
+    private static final String PLAYER_FILE_FORMAT = ".plyr";
 
     // path of the photos folder
     private static final String PHOTOS_FOLDER = MAIN_FOLDER + "PHOTOS/";
@@ -27,4 +29,48 @@ public class DataBase
     private static final String WALL_IMAGES = PHOTOS_FOLDER + "WALLS/";
     private static final String AMMO_IMAGES = PHOTOS_FOLDER + "AMMOS/";
     private static final String AWARD_IMAGES = PHOTOS_FOLDER + "AWARDS/";
+
+
+
+
+
+
+
+
+
+
+            /*  Methods  */
+
+    /**
+     * This method checks that is there any player with given username or not
+     * 
+     * 
+     * @param usernameToCheck : nwe username to check
+     * @return {@code true} if there is any player with the same username. 
+     * 
+     * @throws IOException if can not open players object file
+     */
+    public static boolean isRegistered(String usernameToCheck) throws IOException
+    {
+        File players = new File(PLAYERS_FOLDER);
+
+
+        ObjectInputStream ois = null; 
+        Player holdToCheck = null;
+        for (File player : players.listFiles())
+        {
+            ois = new ObjectInputStream(new FileInputStream(player));
+
+
+            try{ holdToCheck = (Player) ois.readObject(); }
+            catch (ClassNotFoundException e) {holdToCheck = null; }
+            finally { ois.close(); }
+
+            if (holdToCheck.getUsername().equals(usernameToCheck))
+                return true;
+        }
+
+
+        return false;
+    }
 }
