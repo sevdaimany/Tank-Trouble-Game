8fa9169c2a3aa13584f06866b524befd4aa9630e
sevda <imanysevda@gmail.com>
2020-07-27 13:51:50 +0430

    create RegisterView class

---
+++ RegisterView.java
@@ -0,0 +1,44 @@
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+
+/**
+ * this class represent register page's view
+ * @author Sevda Imany
+ * @version 0.0.0
+ */
+public class RegisterView extends GridPane {
+
+    /* Fields */
+    
+    // username's text field
+    private TextField username;
+
+    // password's text field
+    private TextField password;
+
+    // repeat password's text field
+    private TextField repeatPassword;
+
+    // firstname's text field
+    private TextField firstname;
+
+    // lastname's text field
+    private TextField lastname;
+
+    // register's button
+    private Button registerBTN;
+
+
+    /* Constructor */
+
+
+    /**
+     * create register page's view
+     */
+    public RegisterView() {
+
+    }
+
+}
