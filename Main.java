3c0ac32411df1f1b3909eabef2acf4b49ac05dda
sevda <imanysevda@gmail.com>
2020-07-27 13:41:39 +0430

    add start method

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 
 import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
 
 /**
  * Main class represent  tank trouble game
@@ -16,6 +18,29 @@ public class Main extends Application {
         launch(args);
     }
 
+
+
+        /* Methods */
+
+     /**
+     * @param primaryStage
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        MenuController menuController = new MenuController(primaryStage);
+        //Creating a scene object
+        Scene scene = new Scene(menuController.getMenuview());
+        //Adding scene to the stage
+        primaryStage.setScene(scene);
+        //setting stage's min size
+        primaryStage.setMinHeight(800);
+        primaryStage.setMinWidth(1480);
+        //Setting title to the Stage
+        primaryStage.setTitle("Tank Trouble");
+        //Displaying the contents of the stage
+        primaryStage.show();
+    }
 
     
 }
