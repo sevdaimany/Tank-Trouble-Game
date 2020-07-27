3d208536b04b6bdd0d0ba09d9aae39fc76c31aa0
sevda <imanysevda@gmail.com>
2020-07-27 13:37:09 +0430

    add javadoc

--- MenuController.java
+++ MenuController.java
@@ -2,11 +2,28 @@
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * this class represent  menu page's controller
+ * 
+ * @author Sevda Imany
+ * @version 0.0.0
+ */
 public class MenuController {
 
+        /* Fields */
+
+    // menu page's view
     private Menuview menuview;
+    // game 's stage'
     private Stage stage;
 
+
+         /* Constructor */
+
+    /**
+     * creat menu controller 
+     * @param stage
+     */
     public MenuController(Stage stage){
         try {
             menuview = new Menuview();
@@ -17,6 +34,13 @@ public class MenuController {
         this.stage = stage;
     }
 
+
+        /* Methods */
+
+
+    /**
+     *this method add register button's action listener
+     */
     public void registerBTNAction(){
         RegisterController registerController = new RegisterController();
         menuview.getRegisterBTN().setOnAction(event -> {
@@ -25,6 +49,11 @@ public class MenuController {
         });
     }
 
+    // * getter methods *
+
+    /**
+     * @return menu page's view
+     */
     public Menuview getMenuview() {
         return menuview;
     }
