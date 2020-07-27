a81fda5e374365e78678a7081481079ddf54a1dc
sevda <imanysevda@gmail.com>
2020-07-27 13:39:34 +0430

    creat Main class

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+import javafx.application.Application;
+
+/**
+ * Main class represent  tank trouble game
+ *
+ * @author Sevda Imany
+ * @version 0.0.0
+ */
+public class Main extends Application {
+
+
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+
+
+    
+}
