0d1482cce5c0a1252a9f2d3c1a305c301a2aa127
sevda <imanysevda@gmail.com>
2020-07-29 12:13:25 +0430

    created fields

--- LoginView.java
+++ LoginView.java
@@ -1,27 +1,79 @@
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
+import javafx.geometry.Pos;
 
 
-import java.io.FileInputStream;
-
+/**
+ * this class represent login page
+ * 
+ * @author Sevda Imany
+ * @version 0.0.0
+ */
 public class LoginView extends BorderPane {
+
+            /*  Fields  */
+
+
+    //player's username
     private TextField username;
+
+    //player's password
     private TextField password;
+
+    //login button
     private Button loginBTN;
 
+            /*  Constructor  */
+
+
     public LoginView() {
+
+        //create username field
+        username = new TextField();
+        username.setPromptText("username");
+        username.setAlignment(Pos.CENTER);
+
+        //create password field
+        password = new TextField();
+        password.setPromptText("password");
+        password.setAlignment(Pos.CENTER);
+
+        //create login button
+        loginBTN = new Button("Login");
+        loginBTN.setAlignment(Pos.CENTER);
+        loginBTN.setMinWidth(70);
+
       
     }
 
+
+            /*  Methods  */
+
+    // * getter methods *
+
+
+
+    /**
+     * @return player's username
+     */
     public TextField getUsername() {
         return username;
     }
 
+
+    /** 
+     * @return player's password
+     */
     public TextField getPassword() {
         return password;
     }
 
+
+
+    /**
+     * @return login button
+     */
     public Button getLoginBTN() {
         return loginBTN;
     }
