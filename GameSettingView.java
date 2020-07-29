6bb68312f163ffb0185f748cff3618218a272cef
sevda <imanysevda@gmail.com>
2020-07-29 13:10:54 +0430

    create nodes

--- GameSettingView.java
+++ GameSettingView.java
@@ -28,6 +28,41 @@ public class GameSettingView extends BorderPane {
          /* Constructor */
 
     public GameSettingView() {
-     
+
+
+        //tank's health lable
+        Label tankesHealth = new Label("The number of tank's health  :  ");
+        tankesHealth.setFont(Font.font("normal" , FontWeight.BOLD , FontPosture.REGULAR, 15));
+        tankesHealth.setBackground(new Background(new BackgroundFill(Color.KHAKI, CornerRadii.EMPTY, Insets.EMPTY)));
+        tankesHealthTextField = new TextField();
+        HBox tank = new HBox(tankesHealth,tankesHealthTextField);
+        tank.setSpacing(5);
+
+
+        //bullet's power lable
+        Label bulletPower = new Label("The number of bullet's power : ");
+        bulletPower.setBackground(new Background(new BackgroundFill(Color.KHAKI, CornerRadii.EMPTY, Insets.EMPTY)));
+        bulletPower.setFont(Font.font("normal" ,FontWeight.BOLD ,FontPosture.REGULAR, 15));
+        bulletPowerTextField = new TextField();
+        HBox bullet = new HBox(bulletPower,bulletPowerTextField);
+        bullet.setSpacing(5);
+
+        
+        //wall's health lable
+        Label wallHealth = new Label("The number of wall's health  :  ");
+        wallHealth.setBackground(new Background(new BackgroundFill(Color.KHAKI, CornerRadii.EMPTY, Insets.EMPTY)));
+        wallHealth.setFont(Font.font("normal" ,FontWeight.BOLD ,FontPosture.REGULAR, 15));
+        wallHealthTextField = new TextField();
+        HBox wall = new HBox(wallHealth,wallHealthTextField);
+        wall.setSpacing(5);
+
+
+        //game setting text
+        Text gameSettingTXT = new Text("Game Setting");
+        gameSettingTXT.setFill(Color.ORANGE);
+        gameSettingTXT.setStroke(Color.BLACK);
+        gameSettingTXT.setFont(Font.font("Verdana", FontWeight.BOLD, FontPosture.REGULAR, 50));
+        gameSettingTXT.setStrokeWidth(2);
+
     }
 }
