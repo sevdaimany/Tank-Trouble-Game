94a898d38ac5ea942de402314d882c8b72f5141f
MmahdiM79 <m.mahdi.m79@gmail.com>
2020-07-29 10:42:16 +0430

    two new methods added for create player's tank and controller

--- Player.java
+++ Player.java
@@ -10,7 +10,7 @@
  * 
  * 
  * @author Mohammad Mahdi Malmasi
- * @version 0.0.0
+ * @version 0.0.5
  */
 public class Player extends Identity
 {
@@ -50,5 +50,52 @@ public class Player extends Identity
         score = 0;
         playerTank = null;
         playerController = null;
+    }
+
+
+
+
+
+
+
+
+
+            /*  Methods  */
+
+    /**
+     * This method create player's tank.
+     * This method should be call after {@link Player#Player(String, String, String, String)}
+     * 
+     * 
+     * @param imageFilePath : address of the tank image file
+     */
+    public void createPlayerTank(String imageFilePath)
+    {
+        playerTank = new Tank(imageFilePath);
+    }
+
+
+    /**
+     * This method create player's controller
+     * This method should be call after {@link Player#createPlayerTank(String)}
+     * 
+     *  
+     * @param playerTank : player's {@code Tank}
+     * @param moveAheadKey : code of the key that moves tank ahead
+     * @param moveBackwardsKey : code of the key that moves tank backwards
+     * @param turnRightKey : code of the key that turns tank to the right
+     * @param turnLeftKey : code of the key that turns tank to the left
+     * 
+     * @throws NullPointerException if player's tank not created. before this method, you should call {@link Player#createPlayerTank(String)}
+     */
+    public void createPlayerController(int moveAheadKey, int moveBackwardsKey, 
+                                       int turnRightKey, int turnLeftKey) throws NullPointerException 
+    {
+        if (playerTank == null)
+            throw new NullPointerException("player's tank not created");
+
+        playerController = new Controller(playerTank, 
+                                            moveAheadKey, moveBackwardsKey, 
+                                            turnRightKey, turnLeftKey);
     }
 }
