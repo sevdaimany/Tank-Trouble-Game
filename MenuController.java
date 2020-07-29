00c1c89782268d68450ccb88c40d700c1fd975bf
MmahdiM79 <m.mahdi.m79@gmail.com>
2020-07-29 09:20:41 +0430

    minnor changes.(persian: khoshgelasion)

--- MenuController.java
+++ MenuController.java
@@ -1,32 +1,53 @@
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
+
+
+
+
 /**
  * this class represent  menu page's controller
  * 
+ * 
+ * 
  * @author Sevda Imany
- * @version 0.0.0
+ * @version 0.0.2
  */
-public class MenuController {
+public class MenuController 
+{
 
-        /* Fields */
+            /*  Fields  */
 
     // menu page's view
     private Menuview menuview;
+
     // game 's stage'
     private Stage stage;
+
+
+
+
+
+
+
 
 
          /* Constructor */
 
     /**
-     * creat menu controller 
+     * create menu controller
+     * 
+     *  
      * @param stage
      */
-    public MenuController(Stage stage){
-        try {
+    public MenuController(Stage stage)
+    {
+        try
+        {
             menuview = new Menuview();
-        }catch (Exception e){}
+        }
+        catch (Exception e){}
 
         registerBTNAction();
 
@@ -34,26 +55,38 @@ public class MenuController {
     }
 
 
-        /* Methods */
 
 
-    /**
-     *this method add register button's action listener
-     */
-    public void registerBTNAction(){
-        RegisterController registerController = new RegisterController();
-        menuview.getRegisterBTN().setOnAction(event -> {
-            stage.setScene(new Scene(registerController.getRegisterView()));
-            stage.setTitle("Register");
-        });
-    }
+
+
+
+
+
+            /*  Methods  */
+
 
     // * getter methods *
 
     /**
      * @return menu page's view
      */
-    public Menuview getMenuview() {
-        return menuview;
-    }
+    public Menuview getMenuview() { return menuview; }
+
+
+
+
+
+    /**
+     * This method add register button's action listener
+     */
+    public void registerBTNAction()
+    {
+        RegisterController registerController = new RegisterController();
+
+        menuview.getRegisterBTN().setOnAction(event -> 
+        {
+            stage.setScene(new Scene(registerController.getRegisterView()));
+            stage.setTitle("Register");
+        });
+    }    
 }
