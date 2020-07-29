73a73d51eb40f3658a53202bbd04cb2f5df78286
sevda <imanysevda@gmail.com>
2020-07-29 13:29:34 +0430

    add settingView class

---
+++ SettingView.java
@@ -0,0 +1,34 @@
+
+import javafx.geometry.Rectangle2D;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.stage.Screen;
+import java.io.FileInputStream;
+/**
+ * this class represent setting page's view
+ * 
+ * @author Sevda Imany
+ * @version 0.0.0
+ */
+public class SettingView extends TabPane {
+
+            /*  Fields  */
+
+    //loginController object
+    private LoginController loginController;
+
+    //GameSettingController object
+    private GameSettingController gameSettingController;
+
+
+         /* Constructor */
+
+    /**
+     * create setting view page
+     */
+    public SettingView() {
+       
+    }
+
+}
