42df84941c2142838225658c38bb1fbef0837e6c
sevda <imanysevda@gmail.com>
2020-07-29 13:07:47 +0430

    add gameSettingView class

---
+++ GameSettingView.java
@@ -0,0 +1,33 @@
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+
+/**
+ * this class represent gameSetting page's view
+ * 
+ * @author Sevda Imany
+ * @version 0.0.0
+ */
+public class GameSettingView extends BorderPane {
+
+            /*  Fields  */
+
+
+    private TextField tankesHealthTextField;
+    private TextField bulletPowerTextField;
+    private TextField wallHealthTextField;
+
+
+         /* Constructor */
+
+    public GameSettingView() {
+     
+    }
+}
