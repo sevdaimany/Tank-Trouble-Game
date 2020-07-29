047a2150f25813b40f82a77624b7e3995d72e680
sevda <imanysevda@gmail.com>
2020-07-29 12:42:09 +0430

    create userSettingView class

---
+++ UserSettingView.java
@@ -0,0 +1,42 @@
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Screen;
+import java.io.FileInputStream;
+
+/**
+ * this class represent usersetting page's view
+ * 
+ * @author Sevda Imany
+ * @version 0.0.0
+ */
+
+public class UserSettingView extends BorderPane{
+
+            /*  Fields  */
+
+    
+    private Label answerHourLable;
+    private Label answerNameLable;
+    private Label answerNumWinComputer;
+    private Label answerNumFailureComputer;
+    private Label answerNumWinServer;
+    private Label answerNumFailureServer;
+    private Button tankButton;
+
+
+    public UserSettingView(){
+
+
+      
+    }
+}
