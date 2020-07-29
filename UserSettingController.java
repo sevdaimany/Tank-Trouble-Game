c4cdf5a78ce67bbc9dda6695182d7efabaaa9fcb
sevda <imanysevda@gmail.com>
2020-07-29 12:55:36 +0430

    add userSettingController class

---
+++ UserSettingController.java
@@ -0,0 +1,36 @@
+import javafx.stage.Stage;
+
+/**
+ * this class represent userSetting page's controller
+ * 
+ * @author Sevda Imany
+ * @version 0.0.0
+ */
+public class UserSettingController {
+
+            /*  Fields  */
+
+    private  UserSettingView userSettingView;
+    private Stage stage;
+
+
+         /* Constructor */
+
+    public UserSettingController() {
+        userSettingView = new UserSettingView();
+        this.stage =StageConnection.getStage();
+    }
+
+
+          /*  Methods  */
+
+
+    // * getter methods *
+
+    /**
+     * @return userSetting's view
+     */
+    public UserSettingView getUserSettingView() {
+        return userSettingView;
+    }
+}
