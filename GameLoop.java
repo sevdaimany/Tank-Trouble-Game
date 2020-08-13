f313d1ca5c43635e1b78f2fae947cb2f5f975de4
sevda <imanysevda@gmail.com>
2020-08-13 11:29:11 +0430

    add GameLoop class

---
+++ GameLoop.java
@@ -0,0 +1,33 @@
+
+import javafx.animation.AnimationTimer;
+
+public class GameLoop {
+
+    private GameView gameView;
+
+    
+    public void init(GameView gameView) {
+        this.gameView = gameView;
+        GameState.init();
+        run();
+
+    }
+
+
+    public void run() {
+
+        final AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long timestamp) {
+                GameState.updateTankState();
+                gameView.getGamePlayGround().render();
+            }
+        };
+        timer.start();
+    }
+}
+
+
+
+
+
