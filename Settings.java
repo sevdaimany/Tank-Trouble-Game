c81418d2f3123ff3e77e5bbbb3ba615200f63116
MmahdiM79 <m.mahdi.m79@gmail.com>
2020-08-13 09:54:51 +0430

    Settings class added. version 0.0.0

---
+++ Settings.java
@@ -0,0 +1,20 @@
+
+
+
+
+/**
+ * This class holds game settings fields
+ * 
+ * @author Mohammad Mahdi Malmasi
+ * @version 0.0.0
+ */
+public final class Settings 
+{
+            /*  Fields  */
+
+    // tank's health
+    int tanksHealth;
+
+    // woody walls health
+    int woodWallsHealth;
+}
