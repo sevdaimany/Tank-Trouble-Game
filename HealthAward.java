0db1a94c472ac4c54b59ef065dae1f5459de4e29
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-08-15 11:10:48 +0430

    Health class added version 0.0.0

---
+++ HealthAward.java
@@ -0,0 +1,47 @@
+
+
+
+
+/**
+ * This class represent a health reward in game 
+ * 
+ * 
+ * @author Mohammad Mahdi Malmasi
+ * @version 0.0.0
+ */
+public class HealthAward extends Reward
+{
+            /*  Fields  */
+
+    // amount of health that this reward given to tank
+    private int health;
+
+
+
+
+
+
+         /* Constructor */
+
+    /**
+     * Create a new health reward with given details
+     * 
+     * 
+     * @param imageFilePath : address of reward image file
+     * @param x : x of the reward
+     * @param y : y of the reward
+     * @param lifeTime : visibility time of this reward
+     * @param health : amount of health that this award gives to tank
+     */
+    public HealthAward(String imageFilePath, float x, float y, float lifeTime, int health)
+    {
+        super(imageFilePath, x, y, lifeTime);
+        this.health = health;
+    }
+
+
+    public void applyRewardToTank(Tank tankToApply) 
+    {
+        tankToApply.healthDelta(health);
+    }
+}
