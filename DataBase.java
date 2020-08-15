f12ebb7b49f072c3fb4b6e805aee08d8c28365c6
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-08-15 12:48:41 +0430

    ground github download link added

--- DataBase.java
+++ DataBase.java
@@ -13,7 +13,7 @@ import java.util.*;
  * 
  * 
  * @author Mohammad Mahdi Malmasi
- * @version 0.14.4
+ * @version 0.14.5
  */
 public class DataBase
 {
@@ -100,6 +100,8 @@ public class DataBase
     private static final String explosionsGithubLink = GITHUB_SERVER + EXPLOSION_IMAGES;
 
     private static final String awardsGithubLink = GITHUB_SERVER + AWARD_IMAGES;
+
+    private static final String groundsGithubLink = GITHUB_SERVER + GROUND_IMAGES;
 
 
 
