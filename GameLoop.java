8596164e65294e2f3bbc4318e551bbe73b6a68c6
sevda <imanysevda@gmail.com>
2020-09-08 23:19:51 +0430

    add scores method

--- GameLoop.java
+++ GameLoop.java
@@ -1,6 +1,22 @@
 
 import javafx.animation.AnimationTimer;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 
+/**
+ * This class represent the main game loop
+ * 
+ * 
+ * @author Sevda Imany
+ * 
+ * @version 0.0.0
+ */
 public class GameLoop 
 {
 
@@ -46,6 +62,54 @@ public class GameLoop
             }
         }
         return  null;
+    }
+
+
+
+    /**
+     * @return scores Vbox
+     */
+    private VBox scores(){
+        VBox vBox = new VBox();
+        vBox.setAlignment(Pos.CENTER);
+
+        Label gameOverLabel = new Label("GAME OVER");
+        gameOverLabel.setAlignment(Pos.CENTER);
+        gameOverLabel.setFont(Font.font("Verdana", FontWeight.BOLD, FontPosture.REGULAR, 80));
+        gameOverLabel.setTextFill(Color.DARKORANGE);
+
+
+
+        Label winnerLable = new Label("WINNER : " +winner().getUsername());
+        winnerLable.setFont(Font.font("Verdana", FontWeight.NORMAL, FontPosture.REGULAR, 40));
+
+
+        Label scoreLabel = new Label("SCORES");
+        scoreLabel.setFont(Font.font("Verdana", FontWeight.NORMAL, FontPosture.REGULAR, 40));
+
+        vBox.getChildren().add(gameOverLabel);
+        vBox.getChildren().add(new Label("          "));
+        vBox.getChildren().add(winnerLable);
+        vBox.getChildren().add(new Label("          "));
+        vBox.getChildren().add(scoreLabel);
+
+
+        for (Player player : GameState.getPlayers()){
+
+            Label username = new Label(player.getUsername()+" : ");
+            username.setFont(Font.font("Verdana", FontWeight.NORMAL, FontPosture.REGULAR, 20));
+
+            Label score = new Label(String.valueOf(player.getPlayerTank().getHealth()));
+            score.setFont(Font.font("Verdana", FontWeight.NORMAL, FontPosture.REGULAR, 20));
+
+            HBox hBox = new HBox(username , score);
+            hBox.setAlignment(Pos.CENTER);
+            vBox.getChildren().add(hBox);
+            vBox.setOpacity(1);
+
+
+        }
+        return vBox;
     }
 
 }
