d0b452019062b113236eb493d111b646d715b36b
sevda <imanysevda@gmail.com>
2020-09-14 20:53:39 +0430

    chnaged DamageAward class

--- DamageAward.java
+++ DamageAward.java
@@ -12,46 +12,16 @@
  */
 public class DamageAward extends Award
 {
-            /*  Fields  */
-
-    // amount of damage to give tank's ammos
-    private int damage;
-
-
-
-
-
-
-
-
-         /* Constructor */
-
-    /**
-     * Create a new damage reward with given details
-     * 
-     * 
-     * @param imageFilePath : address of reward image file
-     * @param x : x of the reward
-     * @param y : y of the reward
-     * @param lifeTime : visibility time of this reward
-     * @param damage : new damage to give to the tanks
-     */
-    public DamageAward(String imageFilePath, float x, float y, float lifeTime, int damage) 
-    {
-        super(imageFilePath, x, y, lifeTime);
-        this.damage = damage;
-    }
-
-
-
-
-
 
 
             /*  Methods  */
 
     public void applyRewardToTank(Tank tankToApply) 
     {
-        
+        while(isAlive()) {
+            tankToApply.getAmmo().setDamage(DataBase.getSettings().ammoDamage * 2);
+        }
+        tankToApply.getAmmo().setDamage(DataBase.getSettings().ammoDamage);
+
     }
 }
