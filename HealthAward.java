7cc2a268f567b4f97c34fb32053db7a5b55501ca
sevda <imanysevda@gmail.com>
2020-09-14 20:37:19 +0430

    changed HealthAward class

--- HealthAward.java
+++ HealthAward.java
@@ -12,43 +12,14 @@
  */
 public class HealthAward extends Award
 {
-            /*  Fields  */
-
-    // amount of health that this reward given to tank
-    private int health;
-
-
-
-
-
-
-         /* Constructor */
-
-    /**
-     * Create a new health reward with given details
-     * 
-     * 
-     * @param imageFilePath : address of reward image file
-     * @param x : x of the reward
-     * @param y : y of the reward
-     * @param lifeTime : visibility time of this reward
-     * @param health : amount of health that this award gives to tank
-     */
-    public HealthAward(String imageFilePath, float x, float y, float lifeTime, int health)
-    {
-        super(imageFilePath, x, y, lifeTime);
-        this.health = health;
-    }
-
-
-
-
-
+     
 
             /*  Methods  */
 
     public void applyRewardToTank(Tank tankToApply) 
     {
-        tankToApply.healthDelta(health);
+        tankToApply.healthDelta((int)(DataBase.getSettings().tanksHealth * 0.1));
+
     }
+
 }
