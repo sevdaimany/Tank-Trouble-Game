6c88a4a9f86a75042375844f20dea32d549b1a39
sevda <imanysevda@gmail.com>
2020-09-14 20:50:19 +0430

    fixed applyRewardToTank method

--- ProtecterAward.java
+++ ProtecterAward.java
@@ -1,11 +1,12 @@
 public class ProtecterAward extends Award {
 
-    
+
     @Override
     public void applyRewardToTank(Tank tankToApply) {
-        while (isAlive()){
-            GameState.getProtectedTanks().add(tankToApply);
-        }
+       long endTime = System.currentTimeMillis() + 15000;
+        GameState.getProtectedTanks().add(tankToApply);
+
+        while (System.currentTimeMillis() < endTime) {}
         GameState.getProtectedTanks().remove(tankToApply);
     }
 
