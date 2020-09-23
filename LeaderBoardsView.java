7b603a819dd49f7a96b80ee85985078bdd0de443
sevda <imanysevda@gmail.com>
2020-09-23 11:17:30 +0330

    add numbers vbox

--- LeaderBoardsView.java
+++ LeaderBoardsView.java
@@ -6,11 +6,20 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 
+/**
+ * this class represent the Leaderboards view of the game
+ * 
+ * @author Sevda Imany
+ * @version 0.0.0
+ */
 public class LeaderBoardsView extends BorderPane {
 
     public LeaderBoardsView(){
 
         VBox username = new VBox();
+
+        VBox numbers = new VBox();
+
 
         try {
             Player[] players = DataBase.getLeaderBoards();
@@ -20,6 +29,13 @@ public class LeaderBoardsView extends BorderPane {
                 playerInfo.setFont(Font.font("Verdana", FontWeight.MEDIUM, FontPosture.REGULAR, 20));
                 username.getChildren().add(new Label("  "));
                 username.getChildren().add(playerInfo);
+
+                Label number = new Label((i+1) + ")           " );
+                number.setAlignment(Pos.BASELINE_LEFT);
+                number.setFont(Font.font("Verdana", FontWeight.MEDIUM, FontPosture.REGULAR, 20));
+                numbers.getChildren().addAll(new Label("  ") , number );
+
+
 
               
 
